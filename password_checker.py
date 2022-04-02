7a47d8f749780e255c2132a6e03bc2c0e876eb58
Bolu Oluwalade <boluabraham@gmail.com>
2022-04-02 15:57:02 -0400

    :technologist: create password length checker

---
+++ password_checker.py
@@ -0,0 +1,13 @@
+from platform import python_version
+
+
+username = input('Enter Username:')
+password = input('Enter Password:')
+
+password_length = len(password)
+hidden_password = '*' * password_length
+
+if password_length > 10:
+    hidden_password = '*' * 10
+
+print(f'your  password {hidden_password} is {password_length} characters long')
